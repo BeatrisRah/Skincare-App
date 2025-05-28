b26b4df44015d87a0dcf70da1281866f9a14b0fe
Beatris <beatrisraj7@gmail.com>
2025-05-28 22:36:38 +0300

    Steps Styling

--- components/sections/TempSection.tsx
+++ components/sections/TempSection.tsx
@@ -23,6 +23,8 @@ export default function TemplateSection({steps, type}: TemplateSectionProps) {
             : type === "Noon"
             ? "bg-[#F7C7A2]"
             : "bg-[#6094D7]";
+        
+        const stepsCont = Object.keys(steps).length;
     
     return (
         <View className='w-11/12 mx-auto mb-5'>
@@ -32,9 +34,9 @@ export default function TemplateSection({steps, type}: TemplateSectionProps) {
             {/* TODO: Button for edit */}
 
             {/* List? View? We will see */}
-            <View className={`${bgColor} min-h-10 rounded`}>
-                {Object.keys(steps).length === 0 ? <Text className='p-2'>No routine added...</Text>:
-                Object.entries(steps).map(([name, steps]) => <Step key={name} title={name} />)}
+            <View className={`${bgColor} min-h-10 rounded p-2`}>
+                {stepsCont === 0 ? <Text className='p-2'>No routine added...</Text>:
+                Object.entries(steps).map(([name, steps], i) => <Step key={name} title={name} index={i} />)}
                 
             </View>
         </View>
@@ -50,11 +52,15 @@ function Header({icon, title}: {icon:React.ReactNode, title:string}){
     )
 }
 
-function Step({title}: {title:string}){
+function Step({title, index}: {title:string, index:number}){
     return(
-        <View>
+        <View className='flex flex-row items-center p-2 mb-2 border-b-2 border-black/50 pb-4'>
             {/* Drop down menu */}
+            <View className="w-9 h-9 border-2 border-black rounded-full items-center justify-center mr-3">
+                <Text className="text-base ">{index + 1}</Text>
+            </View>
             <Text className='capitalize text-xl'>{title}</Text>
         </View>
     )
 }
+
